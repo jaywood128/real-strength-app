d00309bed4e0685f56017fa2e9c10bf3e90d0c97
Johnathon Wood <jaywood128@gmail.com>
2019-03-02 22:19:44 +0000

    Updating

--- app/controllers/lift_controller.rb
+++ app/controllers/lift_controller.rb
@@ -13,19 +13,21 @@ class LiftController < ApplicationController
     @user = current_user
     @user.lifts << lift
     @user.save
-    binding.pry
-    erb :'/lifts/lifts'
+     redirect to "/lifts/#{lift.id }"
   end
 
   get '/lifts' do
   if !logged_in?
     redirect to '/login'
   else
-
     @user = current_user
     @lifts = Lift.all
 erb :'lifts/lifts'
   end
 end
+    get "/lifts/':id" do
+   @lift = Lift.find(params[:id])
+   erb :show_lift
+  end
 
 end
