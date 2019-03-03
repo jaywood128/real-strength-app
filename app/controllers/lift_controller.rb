ede7a0414c46a59f8b53b98bd31f5ae7b840ae1c
Johnathon Wood <jaywood128@gmail.com>
2019-03-03 21:52:23 +0000

    Fixing /lifts/:id - params[:id] record not found

--- app/controllers/lift_controller.rb
+++ app/controllers/lift_controller.rb
@@ -1,21 +1,5 @@
 class LiftController < ApplicationController
 
-  get '/lifts/new' do
-    if logged_in?
-      erb :'lifts/new'
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/lifts/new' do
-    lift = Lift.create(params)
-    @user = current_user
-    @user.lifts << lift
-    @user.save
-     redirect to "/lifts/#{lift.id }"
-  end
-
   get '/lifts' do
   if !logged_in?
     redirect to '/login'
@@ -25,9 +9,29 @@ class LiftController < ApplicationController
 erb :'lifts/lifts'
   end
 end
-    get "/lifts/':id" do
+
+  get '/lifts/new' do
+
+    if logged_in?
+      erb :'lifts/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/lifts' do
+    lift = Lift.create(params)
+    @user = current_user
+    @user.lifts << lift
+    @user.save
+
+     redirect to "/lifts/#{lift.id}"
+  end
+
+    get "/lifts/:id" do
+      @user = current_user
    @lift = Lift.find(params[:id])
-   erb :show_lift
+   redirect to :'/lifts/show_lift'
   end
 
 end
